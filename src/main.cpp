9f324429e8c4446d4c45ff523c70927d17e5a04c
Dorian-LB <dorian.le-boulch@2024.icam.fr>
2024-11-22 14:30:58 +0100

    4 groups of 2 leds, code main

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,98 @@
-// #include "TouchButton.h"
-// #include "RFIDCard.h"
+#include <FastLED.h>
 
-// void setup() {
-//     // Crée un bouton tactile
-//     TouchButton button1(0, 128, CRGB::Red, "blink", 3, 20, 5, 3, 20, 5);
+#define NUM_LEDS 8 // Total number of LEDs
+#define DATA_PIN 16 // Pin to which the LED strip is connected
 
-//     // Crée une carte RFID
-//     RFIDCard card1("123456789", "User1 Access");
-// }
+CRGB leds[NUM_LEDS]; // Array to hold the LED data
 
-// void loop() {
-//     // Met à jour les LEDs d'un bouton tactile
-//     button1.update();
-// }
+// Define the LEDGroup class
+class LEDGroup {
+private:
+    int *indices;   // Indices of LEDs in the group
+    int size;       // Number of LEDs in the group
+    CRGB color;     // Current color
+    uint8_t intensity; // Intensity (brightness)
+    bool blink;     // Blink state
+    bool state;     // Current ON/OFF state for blinking
+    unsigned long lastUpdate; // Time of the last blink update
+    unsigned int blinkInterval; // Blink interval in milliseconds
+
+public:
+    // Constructor
+    LEDGroup(int *ledIndices, int ledCount, unsigned int interval = 500)
+        : indices(ledIndices), size(ledCount), color(CRGB::Black), intensity(255),
+          blink(false), state(true), lastUpdate(0), blinkInterval(interval) {}
+
+    // Set the color of the group
+    void setColor(CRGB newColor) {
+        color = newColor;
+        updateLEDs();
+    }
+
+    // Set the intensity (brightness) of the group
+    void setIntensity(uint8_t newIntensity) {
+        intensity = newIntensity;
+        FastLED.setBrightness(intensity);
+        updateLEDs();
+    }
+
+    // Enable or disable blinking
+    void setBlink(bool enable, unsigned int interval = 500) {
+        blink = enable;
+        blinkInterval = interval;
+    }
+
+    // Update the LEDs (called in the main loop)
+    void update() {
+        if (blink) {
+            unsigned long currentTime = millis();
+            if (currentTime - lastUpdate >= blinkInterval) {
+                state = !state;
+                lastUpdate = currentTime;
+                updateLEDs();
+            }
+        }
+    }
+
+    // Helper to update the LED array
+    void updateLEDs() {
+        for (int i = 0; i < size; i++) {
+            leds[indices[i]] = state ? color : CRGB::Black;
+        }
+        FastLED.show();
+    }
+};
+
+// Define groups
+int group1[] = {0, 1};
+int group2[] = {2, 3};
+int group3[] = {4, 5};
+int group4[] = {6, 7};
+
+LEDGroup ledGroup1(group1, 2);
+LEDGroup ledGroup2(group2, 2);
+LEDGroup ledGroup3(group3, 2);
+LEDGroup ledGroup4(group4, 2);
+
+void setup() {
+    FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.clear();
+
+    // Set initial configuration for groups
+    ledGroup1.setColor(CRGB::Red);
+    ledGroup2.setColor(CRGB::Green);
+    ledGroup3.setColor(CRGB::Blue);
+    ledGroup4.setColor(CRGB::Yellow);
+
+    ledGroup1.setBlink(true, 300);
+    ledGroup2.setBlink(false);
+    ledGroup3.setIntensity(128);
+    ledGroup4.setIntensity(64);
+}
+
+void loop() {
+    ledGroup1.update();
+    ledGroup2.update();
+    ledGroup3.update();
+    ledGroup4.update();
+}
